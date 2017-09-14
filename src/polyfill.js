0d8eafa9131b5ef585d866c3bba6a6adbf00d622
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-09-14 12:12:59 +0200

    Fix eslints.

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,24 +1,21 @@
-(function() {
-  var lastTime = 0;
-  var vendors = ['webkit', 'moz'];
-  for(var x = 0; x < vendors.length && !global.requestAnimationFrame; ++x) {
-    global.requestAnimationFrame = global[vendors[x]+'RequestAnimationFrame'];
-    global.cancelAnimationFrame =
-      global[vendors[x]+'CancelAnimationFrame'] || global[vendors[x]+'CancelRequestAnimationFrame'];
-  }
+let lastTime = 0;
+const vendors = ['webkit', 'moz'];
+for (let x = 0; x < vendors.length && !global.requestAnimationFrame; ++x) {
+  global.requestAnimationFrame = global[`${vendors[x]}RequestAnimationFrame`];
+  global.cancelAnimationFrame =
+    global[`${vendors[x]}CancelAnimationFrame`] || global[`${vendors[x]}CancelRequestAnimationFrame`];
+}
 
-  if (!global.requestAnimationFrame)
-    global.requestAnimationFrame = function(callback) {
-      var currTime = new Date().getTime();
-      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = global.setTimeout(function() { callback(currTime + timeToCall); },
-        timeToCall);
-      lastTime = currTime + timeToCall;
-      return id;
-    };
+if (!global.requestAnimationFrame) {
+  global.requestAnimationFrame = (callback) => {
+    const currTime = new Date().getTime();
+    const timeToCall = Math.max(0, 16 - (currTime - lastTime));
+    const id = global.setTimeout(() => { callback(currTime + timeToCall); }, timeToCall);
+    lastTime = currTime + timeToCall;
+    return id;
+  };
+}
 
-  if (!global.cancelAnimationFrame)
-    global.cancelAnimationFrame = function(id) {
-      clearTimeout(id);
-    };
-}());
+if (!global.cancelAnimationFrame) {
+  global.cancelAnimationFrame = (id) => clearTimeout(id);
+}
